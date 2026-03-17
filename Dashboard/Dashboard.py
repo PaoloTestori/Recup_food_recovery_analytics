ae60e1fa6f18dfff977d69d663fa068c2d2641c1
paolotestori <paolotestori2@gmail.com>
2026-03-17 16:05:42 +0100

    added page overview and dashboard.py handling

---
+++ Dashboard/Dashboard.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+pages = [
+    st.Page("pages/1_Homepage.py", title="Homepage", icon="🍌"),
+    st.Page("pages/2_Overview.py", title="Overview", icon="🍲"),
+    st.Page("pages/3_Mercati.py", title="Mercati", icon="🥠"),
+    st.Page("pages/4_Giornate di mercato.py", title="Giornate di mercato", icon="☀"),
+]
+
+pg = st.navigation(pages, position="sidebar", expanded=True)
+
+pg.run()
