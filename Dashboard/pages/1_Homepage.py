de24985371d02dfc3103123a442234255cbb1da0
paolotestori <paolotestori2@gmail.com>
2026-04-14 22:03:06 +0200

    update ordine mesi e eliminated IT_it

--- Dashboard/pages/1_Homepage.py
+++ Dashboard/pages/1_Homepage.py
@@ -138,7 +138,11 @@ for idx, row in df.iterrows():
 st.session_state["df"] = df
 
 df["ANNO"] = pd.to_datetime(df['DATA'], format='%d-%m-%Y').dt.year
-df["MESE"] = df["DATA"].dt.month_name(locale="it_IT")
+df["MESE"] = df["DATA"].dt.month.map({
+    1: "Gennaio", 2: "Febbraio", 3: "Marzo", 4: "Aprile",
+    5: "Maggio", 6: "Giugno", 7: "Luglio", 8: "Agosto",
+    9: "Settembre", 10: "Ottobre", 11: "Novembre", 12: "Dicembre"
+})
 ordine_mesi = [
     "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
     "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
